ae76a6d2fb18609f3160daef70bed2c661f2f31f
xuge <49184912@qq.com>
2021-04-13 14:04:18 +0800

    init

---
+++ src/popup/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <button @click="click">click</button>
+</template>
+
+<script>
+import HelloWorld from '@/components/HelloWorld.vue';
+
+export default {
+  name: 'App',
+  methods: {
+    click() {
+      let color = 'green';
+      chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        debugger;
+        alert(JSON.stringify(tabs));
+        chrome.tabs.executeScript(tabs[0].id, { code: 'document.body.style.backgroundColor = "' + color + '";' });
+      });
+    },
+  },
+};
+</script>
+
+<style>
+html {
+  width: 400px;
+  height: 400px;
+}
+</style>
